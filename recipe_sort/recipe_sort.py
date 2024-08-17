3bb03ab261f9115ccbfc18f243084faefd1df3b8
Alan Ravikumar <alanravikumar.ajr@gmail.com>
2024-08-17 23:03:53 +1000

    Changed into a single function

--- recipe_sort/recipe_sort.py
+++ recipe_sort/recipe_sort.py
@@ -1,8 +1,24 @@
 
 import json
 
+def sort(input):
+    data = []
+    ingredients = []
+    # Full match
+    f_match = []
+    # Partial match
+    p_match = []
+    # Final List - sorted by cuisine
+    sorted_list = []
+    with open('recipe_database/recipes.json', 'r') as database:
+        data = json.load(database)
+    # INPUT HERE (need to change if its json file
+    with open('recipe_sort/avaliable_ingredients', 'r') as input_txt:
+        ingredients = input_txt.read().splitlines()
+    category = ingredients[0]
+    cuisine = ingredients[1]
 
-def sort_ing(data, ingredients):
+    #Sort by ingredients
     for recipe in data:
         recipe_ing = recipe.get("ingredient")
         counter = 0
@@ -14,21 +30,30 @@ def sort_ing(data, ingredients):
         elif counter > (len(recipe_ing) / 2):
             p_match.append(recipe)
 
-def sort_category(match_list, category):
+    #Sort by category
     match = []
-    for recipe in match_list:
+    for recipe in f_match:
         category_list = recipe.get("category")
         for r_category in category_list:
             if r_category == category:
                 match.append(recipe)
                 break
-    return match
+    f_match = match
 
-def sort_cuisine(cuisine_list, f_match, p_match):
+    match = []
+    for recipe in p_match:
+        category_list = recipe.get("category")
+        for r_category in category_list:
+            if r_category == category:
+                match.append(recipe)
+                break
+    p_match = match
+
+    #Sort by cuisine
     with open('recipe_database/cuisine.csv', 'r') as cuisines_file:
         cuisines = cuisines_file.read().splitlines()
-    for cuisine in cuisines:
-        if cuisine == 'cuisine':
+    for cuisine_type in cuisines:
+        if cuisine_type == 'cuisine':
             continue
         f_match_c = []
         p_match_c = []
@@ -42,38 +67,4 @@ def sort_cuisine(cuisine_list, f_match, p_match):
         match_list.append(cuisine)
         match_list.append(f_match_c)
         match_list.append(p_match_c)
-        cuisine_list.append(match_list)
-
-
-
-
-
-data = []
-ingredients = []
-#Full match
-f_match = []
-#Partial match
-p_match = []
-#Final List - sorted bu cuisine
-cuisine_list = []
-with open('recipe_database/recipes.json', 'r') as database:
-    data = json.load(database)
-with open('recipe_sort/avaliable_ingredients', 'r') as input_txt:
-    ingredients = input_txt.read().splitlines()
-
-sort_ing(data, ingredients)
-#print(f_match)
-f_match = sort_category(f_match, 'dessert')
-p_match = sort_category(p_match, 'dessert')
-database.close()
-sort_cuisine(cuisine_list, f_match, p_match)
-#print(f_match)
-for c in cuisine_list:
-    print(f"{c[0]}:")
-    print("FULL MATCH:")
-    for f in c[1]:
-        print(f)
-    print("PARTIAL MATCH:")
-    for p in c[2]:
-        print(p)
-input_txt.close()
+        sorted_list.append(match_list)
