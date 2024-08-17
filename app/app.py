9b20dbed0981ea51ee50ae87f6464884679d40c4
Reo906 <142962348+Reo906@users.noreply.github.com>
2024-08-17 14:23:58 +1000

    Add receipt scanner to app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,38 @@
-from receipt_scanner import scan_receipt
 from flask import Flask, request, jsonify
 import io
+import google.generativeai as genai
+import PIL.Image
+import pandas as pd
+
 
 app = Flask(__name__)
 
+def scan_receipt(image_path):
+    df_ingredient = pd.read_csv('recipe_database/ingredient.csv')
+    ingredients = str(df_ingredient['ingredient'].tolist())
+    
+    image = PIL.Image.open(image_path)
+    prompt = f"""
+List product names on the receipt from products listed in this list. If product is not in the list, skip it.
+
+{ingredients}
+
+Only return the type of product, remove the description
+
+Example output:
+apple
+banana
+salt
+"""
+    
+    genai.configure(api_key='AIzaSyCIxUA9BOYIgQRnRFdq7IkvOv_TS3lF3NI')
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    
+    response = model.generate_content([prompt, image])
+    
+    return response.text.lower().strip()
+  
+  
 @app.route('/scan', methods=['POST'])
 def scan():
     if 'file' not in request.files:
